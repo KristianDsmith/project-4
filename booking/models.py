f24b792ba85459f10e4489002d8f5a3da09fda3e
KristianDsmith <>
2023-07-28 11:44:15 +0000

    Add migrate of models for item id

--- booking/models.py
+++ booking/models.py
@@ -7,6 +7,7 @@ class MenuItem(models.Model):
     image_url = models.URLField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     dietary_tags = models.CharField(max_length=100)
+    item_id = models.AutoField(primary_key=True, default=1)
 
     def __str__(self):
         return self.name
